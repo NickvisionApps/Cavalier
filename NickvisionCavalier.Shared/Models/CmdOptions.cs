081f39e1f150e43272747bcb7a1d2ff9600c7211
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-07-26 02:45:57 +0300

    Shared - Sort Cmd options as they appear in the UI

--- NickvisionCavalier.Shared/Models/CmdOptions.cs
+++ NickvisionCavalier.Shared/Models/CmdOptions.cs
@@ -7,35 +7,25 @@ namespace NickvisionCavalier.Shared.Models;
 /// </summary>
 public class CmdOptions {
     /// <summary>
+    /// Active drawing mode
+    /// </summary>
+    [Option('d', "drawing-mode", Required = false, HelpText = "Drawing mode, one of: wavebox, levelsbox, particlesbox, spinebox, barsbox")]
+    public DrawingMode? Mode { get; set; }
+    /// <summary>
+    /// Mirror mode
+    /// </summary>
+    [Option('m', "mirror-mode", Required = false, HelpText = "Mirror more, one of: off, full, splitchannels (stereo only)")]
+    public Mirror? Mirror { get; set; }
+    /// <summary>
+    /// Whether to reverse mirrored bars
+    /// </summary>
+    [Option('v', "reverse-mirror", Required = false, HelpText = "Reverse mirror (true or false)")]
+    public bool? ReverseMirror { get; set; }
+    /// <summary>
     /// Size of drawing area margins in pixels
     /// </summary>
     [Option('n', "margin", Required = false, HelpText = "Area margin (0-40)")]
     public uint? AreaMargin { get; set; }
-    /// <summary>
-    /// Whether the window should be borderless
-    /// </summary>
-    [Option('w', "borderless", Required = false, HelpText = "Make window borderless (true or false)")]
-    public bool? Borderless { get; set; }
-    /// <summary>
-    /// Whether the corners of the window should be sharp
-    /// </summary>
-    [Option('s', "sharp-corners", Required = false, HelpText = "Make window corneres sharp (true or false)")]
-    public bool? SharpCorners { get; set; }
-    /// <summary>
-    /// Number of bar pairs in CAVA
-    /// </summary>
-    [Option('b', "bars", Required = false, HelpText = "Number of bar pairs (3-25)")]
-    public uint? BarPairs { get; set; }
-    /// <summary>
-    /// Whether to set channels to stereo (mono if false)
-    /// </summary>
-    [Option('c', "stereo", Required = false, HelpText = "Mono (false) or stereo (true)")]
-    public bool? Stereo { get; set; }
-    /// <summary>
-    /// Whether to reverse bars order for each channel
-    /// </summary>
-    [Option('e', "reverse-order", Required = false, HelpText = "Reverse order of bars (true or false)")]
-    public bool? ReverseOrder { get; set; }
     /// <summary>
     /// Drawing direction
     /// </summary>
@@ -62,20 +52,30 @@ public class CmdOptions {
     [Option('l', "lines-thickness", Required = false, HelpText = "Lines thickness when filling is off (0-100)")]
     public uint? LinesThickness { get; set; }
     /// <summary>
-    /// Active drawing mode
+    /// Whether the window should be borderless
     /// </summary>
-    [Option('d', "drawing-mode", Required = false, HelpText = "Drawing mode, one of: wavebox, levelsbox, particlesbox, spinebox, barsbox")]
-    public DrawingMode? Mode { get; set; }
+    [Option('w', "borderless", Required = false, HelpText = "Make window borderless (true or false)")]
+    public bool? Borderless { get; set; }
     /// <summary>
-    /// Mirror mode
+    /// Whether the corners of the window should be sharp
     /// </summary>
-    [Option('m', "mirror-mode", Required = false, HelpText = "Mirror more, one of: off, full, splitchannels (stereo only)")]
-    public Mirror? Mirror { get; set; }
+    [Option('s', "sharp-corners", Required = false, HelpText = "Make window corneres sharp (true or false)")]
+    public bool? SharpCorners { get; set; }
     /// <summary>
-    /// Whether to reverse mirrored bars
+    /// Number of bar pairs in CAVA
     /// </summary>
-    [Option('v', "reverse-mirror", Required = false, HelpText = "Reverse mirror (true or false)")]
-    public bool? ReverseMirror { get; set; }
+    [Option('b', "bars", Required = false, HelpText = "Number of bar pairs (3-25)")]
+    public uint? BarPairs { get; set; }
+    /// <summary>
+    /// Whether to set channels to stereo (mono if false)
+    /// </summary>
+    [Option('c', "stereo", Required = false, HelpText = "Mono (false) or stereo (true)")]
+    public bool? Stereo { get; set; }
+    /// <summary>
+    /// Whether to reverse bars order for each channel
+    /// </summary>
+    [Option('e', "reverse-order", Required = false, HelpText = "Reverse order of bars (true or false)")]
+    public bool? ReverseOrder { get; set; }
     /// <summary>
     /// Active color profile index
     /// </summary>
