810f0d928b7783b5df6380d4800f6c12422d0ee0
Nick Logozzo <nlogozzo225@gmail.com>
2025-01-15 14:15:19 -0500

    Shared - Add `Cava` Model

---
+++ libcavalier/include/models/cava.h
@@ -0,0 +1,70 @@
+#ifndef CAVA_H
+#define CAVA_H
+
+#include <filesystem>
+#include <string>
+#include <thread>
+#include <vector>
+#include <libnick/events/parameventargs.h>
+#include <libnick/system/process.h>
+#include "cavaoptions.h"
+
+namespace Nickvision::Cavalier::Shared::Models
+{
+    /**
+     * @brief A model of a cava process.
+     */
+    class Cava
+    {
+    public:
+        /**
+         * @brief Constructs a Cava.
+         * @param options The options to use for cava
+         * @param appName The name of the application
+         */
+        Cava(const CavaOptions& options, const std::string& appName);
+        /**
+         * @brief Destructs a Cava.
+         */
+        ~Cava();
+        /**
+         * @brief Gets the event for when output is received from cava.
+         * @return The output received event
+         */
+        Events::Event<Events::ParamEventArgs<std::vector<float>>>& outputReceived();
+        /**
+         * @brief Gets the options to use for cava.
+         * @return The cava options
+         */
+        const CavaOptions& getOptions() const;
+        /**
+         * @brief Sets the options to use for cava.
+         * @brief This method will restart the cava process if it is running.
+         * @param options The new cava options
+         */
+        void setOptions(const CavaOptions& options);
+        /**
+         * @brief Starts the cava process.
+         * @brief If the cava process has been already started, it will be restarted (loading the configuration file again).
+         */
+        void start();
+
+    private:
+        /**
+         * @brief Writes the cava options to the config file on disk.
+         */
+        void updateConfigFile();
+        /**
+         * @brief Watches for output from the cava process.
+         */
+        void watch();
+        CavaOptions m_options;
+        std::filesystem::path m_configPath;
+        std::shared_ptr<System::Process> m_process;
+        std::thread m_watcher;
+        std::string m_oldOutput;
+        Events::Event<Events::ParamEventArgs<std::vector<float>>> m_outputReceived;
+    };
+}
+
+#endif //CAVA_H
---
+++ libcavalier/src/models/cava.cpp
@@ -0,0 +1,110 @@
+#include "models/cava.h"
+#include <fstream>
+#include <libnick/filesystem/userdirectories.h>
+#include <libnick/system/environment.h>
+
+using namespace Nickvision::Events;
+using namespace Nickvision::Filesystem;
+using namespace Nickvision::System;
+
+namespace Nickvision::Cavalier::Shared::Models
+{
+    Cava::Cava(const CavaOptions& options, const std::string& appName)
+        : m_options{ options },
+        m_configPath{ UserDirectories::get(ApplicationUserDirectory::Config, appName) / "cava_config" }
+    {
+        updateConfigFile();
+    }
+
+    Cava::~Cava()
+    {
+        if(m_process && m_process->kill())
+        {
+            m_process->waitForExit();
+        }
+        if(m_watcher.joinable())
+        {
+            m_watcher.join();
+        }
+    }
+
+    Event<ParamEventArgs<std::vector<float>>>& Cava::outputReceived()
+    {
+        return m_outputReceived;
+    }
+
+    const CavaOptions& Cava::getOptions() const
+    {
+        return m_options;
+    }
+
+    void Cava::setOptions(const CavaOptions& options)
+    {
+        m_options = options;
+        updateConfigFile();
+        if(m_process->isRunning())
+        {
+            start();
+        }
+    }
+
+    void Cava::start()
+    {
+        if(m_process && m_process->kill())
+        {
+            m_process->waitForExit();
+        }
+        if(m_watcher.joinable())
+        {
+            m_watcher.join();
+        }
+        m_process.reset();
+        std::vector<std::string> arguments;
+        arguments.push_back("-p");
+        arguments.push_back(m_configPath.string());
+        m_process = std::make_shared<Process>(Environment::findDependency("cava"), arguments);
+        m_process->start();
+        m_watcher = std::thread(&Cava::watch, this);
+    }
+
+    void Cava::updateConfigFile()
+    {
+        std::ofstream file{ m_configPath, std::ios::trunc };
+        file << m_options.toCavaOptionsString() << std::endl;
+    }
+
+    void Cava::watch()
+    {
+        while(m_process->isRunning())
+        {
+            if(m_oldOutput == m_process->getOutput())
+            {
+                continue;
+            }
+            std::string output{ m_oldOutput.size() == 0 ? m_process->getOutput() : m_process->getOutput().substr(m_oldOutput.size()) };
+            m_oldOutput = m_process->getOutput();
+            unsigned int length{ m_options.getNumberOfBars() * 4 };
+            std::vector<float> sample(m_options.getNumberOfBars() * 2);
+            std::vector<char> bytes{ output.begin(), output.begin() + length };
+            for(unsigned int i = 0; i < length; i += 2)
+            {
+                std::uint16_t* byte{ reinterpret_cast<uint16_t*>(&bytes[i]) };
+                sample[i / 2] = *byte / 65535.0f;
+            }
+            if(m_options.getReverseBarOrder())
+            {
+                if(m_options.getChannels() == ChannelType::Stereo)
+                {
+                    int half{ static_cast<int>(sample.size() / 2) };
+                    std::reverse(sample.begin(), sample.begin() + half);
+                    std::reverse(sample.begin() + half, sample.end());
+                }
+                else
+                {
+                    std::reverse(sample.begin(), sample.end());
+                }
+            }
+            m_outputReceived.invoke({ sample });
+        }
+    }
+}
