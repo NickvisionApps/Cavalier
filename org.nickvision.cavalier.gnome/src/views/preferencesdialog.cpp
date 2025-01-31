5d51767a426ffb7841d64caa592e6f97b23c44e2
Nick Logozzo <nlogozzo225@gmail.com>
2025-01-30 23:58:53 -0500

    feat: More GNOME options

--- org.nickvision.cavalier.gnome/src/views/preferencesdialog.cpp
+++ org.nickvision.cavalier.gnome/src/views/preferencesdialog.cpp
@@ -11,7 +11,27 @@ namespace Nickvision::Cavalier::GNOME::Views
         m_controller{ controller }
     {
         //Load
+        DrawingArea drawing{ m_controller->getDrawingArea() };
+        CavaOptions cava{ m_controller->getCavaOptions() };
         adw_combo_row_set_selected(m_builder.get<AdwComboRow>("themeRow"), static_cast<unsigned int>(m_controller->getTheme()));
+        adw_combo_row_set_selected(m_builder.get<AdwComboRow>("drawingModeRow"), static_cast<unsigned int>(drawing.getMode()));
+        adw_combo_row_set_selected(m_builder.get<AdwComboRow>("drawingShapeRow"), static_cast<unsigned int>(drawing.getShape()));
+        adw_combo_row_set_selected(m_builder.get<AdwComboRow>("drawingDirectionRow"), static_cast<unsigned int>(drawing.getDirection()));
+        adw_switch_row_set_active(m_builder.get<AdwSwitchRow>("fillShapeRow"), drawing.getFillShape());
+        adw_combo_row_set_selected(m_builder.get<AdwComboRow>("mirrowModeRow"), static_cast<unsigned int>(drawing.getMirrorMode()));
+        adw_spin_row_set_value(m_builder.get<AdwSpinRow>("drawingMarginRow"), static_cast<double>(drawing.getMargin()));
+        adw_spin_row_set_value(m_builder.get<AdwSpinRow>("drawingXOffset"), static_cast<double>(drawing.getXOffset()));
+        adw_spin_row_set_value(m_builder.get<AdwSpinRow>("drawingYOffset"), static_cast<double>(drawing.getYOffset()));
+        adw_spin_row_set_value(m_builder.get<AdwSpinRow>("itemSpacingRow"), static_cast<double>(drawing.getItemSpacing()));
+        adw_spin_row_set_value(m_builder.get<AdwSpinRow>("itemRoundnessRow"), static_cast<double>(drawing.getItemRoundness()));
+        adw_combo_row_set_selected(m_builder.get<AdwComboRow>("channelsRow"), static_cast<unsigned int>(cava.getChannels()));
+        adw_spin_row_set_value(m_builder.get<AdwSpinRow>("framerateRow"), static_cast<double>(cava.getFramerate()));
+        adw_spin_row_set_value(m_builder.get<AdwSpinRow>("numberOfBarsRow"), static_cast<double>(cava.getNumberOfBars()));
+        adw_switch_row_set_active(m_builder.get<AdwSwitchRow>("reverseBarOrderRow"), cava.getReverseBarOrder());
+        adw_switch_row_set_active(m_builder.get<AdwSwitchRow>("automaticSensitivityRow"), cava.getUseAutomaticSensitivity());
+        adw_spin_row_set_value(m_builder.get<AdwSpinRow>("sensitivityRow"), static_cast<double>(cava.getSensitivity()));
+        adw_switch_row_set_active(m_builder.get<AdwSwitchRow>("monstercatRow"), cava.getUseMonstercatSmoothing());
+        adw_spin_row_set_value(m_builder.get<AdwSpinRow>("nosieReductionRow"), static_cast<double>(cava.getNoiseReductionFactor()));
         //Signals
         m_closed += [&](const EventArgs&){ onClosed(); };
         g_signal_connect(m_builder.get<GObject>("themeRow"), "notify::selected-item", G_CALLBACK(+[](GObject*, GParamSpec* pspec, gpointer data){ reinterpret_cast<PreferencesDialog*>(data)->onThemeChanged(); }), this);
@@ -19,6 +39,12 @@ namespace Nickvision::Cavalier::GNOME::Views
     
     void PreferencesDialog::onClosed()
     {
+        DrawingArea drawing{ m_controller->getDrawingArea() };
+        CavaOptions cava{ m_controller->getCavaOptions() };
+        drawing.setMode(static_cast<DrawingMode>(adw_combo_row_get_selected(m_builder.get<AdwComboRow>("drawingModeRow"))));
+
+        m_controller->setDrawingArea(drawing);
+        m_controller->setCavaOptions(cava);
         m_controller->saveConfiguration();
     }
 
